aafc0387adba10a49a752aba598f590e822d2381
Otabek33 <kebatotabek33@gmail.com>
2023-08-28 12:14:13 +0500

    calculation

---
+++ calculator_projects/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from calculator_projects.users.models import User
+from calculator_projects.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ calculator_projects/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from calculator_projects.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
