48b4fcc9dac2a0c27d9955747ee7b857aade3215
Otabek33 <kebatotabek33@gmail.com>
2023-09-08 12:28:41 +0500

    updated created and delete request done with rest api for stageplan model

--- calculator_projects/apps/stages/urls.py
+++ calculator_projects/apps/stages/urls.py
@@ -5,5 +5,5 @@ from calculator_projects.apps.stages.views import (StagePlanViewSet,
 
 app_name = "projectstageplan"
 router = DefaultRouter()
-router.register(r"stages", StagePlanViewSet, basename="stageplan")
+router.register(r"project-stages", StagePlanViewSet, basename="stageplan")
 urlpatterns = router.urls
---
+++ calculator_projects/static/js/stage_plan.js
@@ -0,0 +1,84 @@
+const csrftoken = $("[name=csrfmiddlewaretoken]").val();
+$("#stage-form-modal").on('submit', function (e) {
+    e.preventDefault()
+    let form = $(this);
+    let actionUrl = form.attr('action');
+    data = form.serialize()
+    $.ajax({
+        type: "POST",
+        url: actionUrl,
+        data: form.serialize(), // serializes the form's elements.
+        success: function () {
+            $(".close").click()
+            localStorage.setItem("msg", "Этап успешно добавлен")
+            window.location.reload();
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+})
+
+
+$(".delete-stagePlan-row").on("click", function (e) {
+    e.preventDefault();
+    let actionUrl = $(this).attr("href")
+    $.ajax({
+        type: "DELETE",
+        url: actionUrl,
+        headers: {
+            "X-CSRFToken": csrftoken
+        },
+        success: function () {
+            localStorage.setItem("msg", "Успешно удален")
+            window.location.reload();
+        },
+        error: function (err) {
+            toastMixin.fire({
+                title: err
+            })
+        }
+    })
+})
+
+$(".edit-row").on("click", function (e) {
+    e.preventDefault();
+    let actionUrl = $(this).attr("href")
+    $("#stage-update-form").attr("action", actionUrl)
+    $.ajax({
+        type: "GET",
+        url: actionUrl,
+        success: function (data) {
+            console.log("stage id how to get")
+            console.log(data)
+            for (var key in data) {
+                $("#stage-update-form").find('[name="' + key + '"]').val(data[key]);
+            }
+        }
+    })
+    // Remove link row from DOM
+    $("#stageUpdateModal").modal('show')
+})
+
+
+$("#stage-update-form").on('submit', function (e) {
+    e.preventDefault()
+    let form = $(this);
+    let actionUrl = form.attr('action')+"/";
+    data = form.serialize()
+    $.ajax({
+        type: "PUT",
+        url: actionUrl,
+        data: form.serialize(), // serializes the form's elements.
+        headers: {
+            "X-CSRFToken": csrftoken
+        },
+        success: function (data) {
+               localStorage.setItem("msg", "Этап успешно обновлен")
+            window.location.reload();
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+})
