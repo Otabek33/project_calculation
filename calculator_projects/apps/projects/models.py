75854296cefef8e32a9260281aa2dbff5c3ba349
Otabek33 <kebatotabek33@gmail.com>
2023-09-18 17:23:50 +0500

    stage list show updated

--- calculator_projects/apps/projects/models.py
+++ calculator_projects/apps/projects/models.py
@@ -285,4 +285,4 @@ class ProjectPlan(models.Model):
 
     def stage_list(self):
         from calculator_projects.apps.stages.models import StagePlan
-        return StagePlan.objects.filter(projectPlan=self.id, deleted_status=False)
+        return StagePlan.objects.filter(projectPlan=self.id, deleted_status=False).order_by('stage_number')
