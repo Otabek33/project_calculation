4c0f7b40c9ecafbb6e25b404114d9d6a1e3dd835
Otabek33 <kebatotabek33@gmail.com>
2023-10-19 10:14:40 +0500

    js print windows done

---
+++ calculator_projects/static/js/print.js
@@ -0,0 +1,87 @@
+function print_password_project() {
+    let stage_navbar = document.querySelector("#stage_navbar")
+    let sidebar = document.querySelector("#sidebar")
+    let link = document.querySelector("#link")
+    let duration_stage_detail = document.querySelector("#duration_stage_detail")
+    let calculation_stage_detail = document.querySelector("#calculation_stage_detail")
+    let total_calculation_project_detail = document.querySelector("#total_calculation_project_detail")
+    let foot_sentence = document.querySelector("#foot_sentence")
+    let footer_detail = document.querySelector("#footer_detail")
+    stage_navbar.style.display = "none"
+    sidebar.style.display = "none"
+    link.style.display = "none"
+    duration_stage_detail.style.display = "none"
+    calculation_stage_detail.style.display = "none"
+    total_calculation_project_detail.style.display = "none"
+    foot_sentence.style.display = "none"
+    footer_detail.style.display = "none"
+    window.print()
+    stage_navbar.style.display = ""
+    sidebar.style.display = ""
+    link.style.display = ""
+    duration_stage_detail.style.display = ""
+    calculation_stage_detail.style.display = ""
+    total_calculation_project_detail.style.display = ""
+    foot_sentence.style.display = ""
+    footer_detail.style.display = ""
+
+}
+
+function print_stage_duration_graph() {
+    let stage_navbar = document.querySelector("#stage_navbar")
+    let sidebar = document.querySelector("#sidebar")
+    let link = document.querySelector("#link")
+    let password_project = document.querySelector("#password_project")
+
+    let calculation_stage_detail = document.querySelector("#calculation_stage_detail")
+    let total_calculation_project_detail = document.querySelector("#total_calculation_project_detail")
+    let foot_sentence = document.querySelector("#foot_sentence")
+    let footer_detail = document.querySelector("#footer_detail")
+    stage_navbar.style.display = "none"
+    sidebar.style.display = "none"
+    link.style.display = "none"
+    password_project.style.display = "none"
+    calculation_stage_detail.style.display = "none"
+    total_calculation_project_detail.style.display = "none"
+    foot_sentence.style.display = "none"
+    footer_detail.style.display = "none"
+    window.print()
+    stage_navbar.style.display = ""
+    sidebar.style.display = ""
+    link.style.display = ""
+    password_project.style.display = ""
+    calculation_stage_detail.style.display = ""
+    total_calculation_project_detail.style.display = ""
+    foot_sentence.style.display = ""
+    footer_detail.style.display = ""
+}
+
+function total_detail_info_project() {
+    let stage_navbar = document.querySelector("#stage_navbar")
+    let sidebar = document.querySelector("#sidebar")
+    let link = document.querySelector("#link")
+    let password_project = document.querySelector("#password_project")
+    let duration_stage_detail = document.querySelector("#duration_stage_detail")
+    let calculation_stage_detail = document.querySelector("#calculation_stage_detail")
+    let foot_sentence = document.querySelector("#foot_sentence")
+    let footer_detail = document.querySelector("#footer_detail")
+    stage_navbar.style.display = "none"
+    sidebar.style.display = "none"
+    link.style.display = "none"
+    duration_stage_detail.style.display = "none"
+    password_project.style.display = "none"
+    calculation_stage_detail.style.display = "none"
+    foot_sentence.style.display = "none"
+    footer_detail.style.display = "none"
+    window.print()
+    stage_navbar.style.display = ""
+    sidebar.style.display = ""
+    link.style.display = ""
+    password_project.style.display = ""
+    calculation_stage_detail.style.display = ""
+
+    foot_sentence.style.display = ""
+    footer_detail.style.display = ""
+    duration_stage_detail.style.display = ""
+
+}
